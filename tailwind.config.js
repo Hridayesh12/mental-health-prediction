1d781424215cd162f7e2a632612c6dc6f689286f
Gaurav Singh <gaurav.s.140102@gmail.com>
2022-04-02 00:09:57 +0530

    Setup frontend, backend and tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		colors: {
+			primary: "#F8A036",
+		},
+		fontFamily: {
+			sans: ["DM Sans", "sans-serif"],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
