4828fbac80a4c3abc2a99de27a1cfb56ff8a686a
SickeningMyth7 <hridayeshpadalkar7@gmail.com>
2022-04-04 07:50:19 +0530

    SelfHelp page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
-
+import { BrowserRouter as Router ,Routes, Route} from "react-router-dom";
+import SelfHelp from "./components/SelfHelp";
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<Router>
+        <Routes>
+          <Route path="/selfhelp" element={<><SelfHelp/></>}/>
+		</Routes>
+		</Router>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
+		screens: {
+			xs: "250px",
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
 		colors: {
 			primary: "#F8A036",
+			white: "#ffffff",
+			grey: "#c4c4c4",
 		},
 		fontFamily: {
 			sans: ["DM Sans", "sans-serif"],
@@ -11,3 +20,4 @@ module.exports = {
 	},
 	plugins: [],
 };
+
